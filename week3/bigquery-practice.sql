c4ad7dd9652979a5dda8e8fdf029fdaf2d6ccbc5
Isaac-Tolu <isaactoluwani30@gmail.com>
2022-02-12 10:30:04 +0100

    Week 3 work so far

---
+++ week3/bigquery-practice.sql
@@ -0,0 +1,57 @@
+-- Creating external table referring to gcs path
+CREATE OR REPLACE EXTERNAL TABLE `composed-facet-339115.nytaxi.external_yellow_tripdata`
+OPTIONS (
+  format = 'parquet',
+  uris = [
+    'gs://dtc_data_lake_composed-facet-339115/raw/yellow_tripdata/2019/*', 
+    'gs://dtc_data_lake_composed-facet-339115/raw/yellow_tripdata/2020/*'
+    ]
+);
+
+-- Check yellow trip data
+SELECT * FROM composed-facet-339115.nytaxi.external_yellow_tripdata limit 10;
+
+-- Create a non partitioned table from external table
+CREATE OR REPLACE TABLE composed-facet-339115.nytaxi.yellow_tripdata_non_partitoned AS
+SELECT * FROM composed-facet-339115.nytaxi.external_yellow_tripdata;
+
+-- Create a partitioned table from external table
+CREATE OR REPLACE TABLE composed-facet-339115.nytaxi.yellow_tripdata_partitoned
+PARTITION BY
+  DATE(tpep_pickup_datetime) AS
+SELECT * FROM composed-facet-339115.nytaxi.external_yellow_tripdata;
+
+-- Impact of partition
+-- Scanning 1.6GB of data
+SELECT DISTINCT(VendorID)
+FROM composed-facet-339115.nytaxi.yellow_tripdata_non_partitoned
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
+
+-- Scanning ~106 MB of DATA
+SELECT DISTINCT(VendorID)
+FROM composed-facet-339115.nytaxi.yellow_tripdata_partitoned
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2019-06-30';
+
+-- Let's look into the partitons
+SELECT table_name, partition_id, total_rows
+FROM `nytaxi.INFORMATION_SCHEMA.PARTITIONS`
+WHERE table_name = 'yellow_tripdata_partitoned'
+ORDER BY total_rows DESC;
+
+-- Creating a partition and cluster table
+CREATE OR REPLACE TABLE composed-facet-339115.nytaxi.yellow_tripdata_partitoned_clustered
+PARTITION BY DATE(tpep_pickup_datetime)
+CLUSTER BY VendorID AS
+SELECT * FROM composed-facet-339115.nytaxi.external_yellow_tripdata;
+
+-- Query scans 1.1 GB
+SELECT count(*) as trips
+FROM composed-facet-339115.nytaxi.yellow_tripdata_partitoned
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2020-12-31'
+  AND VendorID=1;
+
+-- Query scans 864.5 MB
+SELECT count(*) as trips
+FROM composed-facet-339115.nytaxi.yellow_tripdata_partitoned_clustered
+WHERE DATE(tpep_pickup_datetime) BETWEEN '2019-06-01' AND '2020-12-31'
+  AND VendorID=1;
--- week3/notes.md
+++ week3/notes.md
@@ -1,1 +1,32 @@
 # Data Warehouse and BigQuery
+- OLAP vs OLTP
+- What is a Data Warehouse
+- BigQuery
+    - Cost
+    - 
+
+OLTP - Online Transaction Processing
+OLAP - Online Analytical Processing
+
+I'm thinking of OLTP interms of backend databases, used to perform normal CRUD operations which help business operations
+
+While for OLAP, it is designed for storing data intended for analytical operations.. discovering insights
+
+[More on OLTP vs OLAP](https://www.stitchdata.com/resources/oltp-vs-olap/#:~:text=OLTP%20and%20OLAP%3A%20The%20two,historical%20data%20from%20OLTP%20systems.)
+
+A data warehouse is an OLAP solution
+
+BigQuery is a data warehouse solution. 
+
+It's [severless](https://en.wikipedia.org/wiki/Serverless_computing)
+
+BigQuery has the ability to create an External Table on GCS and query from there.. This is different from a native table that is actually stored on BigQuery
+
+## Partitioning in BigQuery
+
+## Clustering in BigQuery
+
+# Partitoning and Clustering
+
+# BigQuery Best Practices
+Our focus is mainly on cost reduction and improving query performance
