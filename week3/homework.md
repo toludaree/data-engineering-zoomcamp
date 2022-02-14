3f6056e6ece8ee8875b13c93fc6515086346a727
Isaac-Tolu <isaactoluwani30@gmail.com>
2022-02-14 16:13:03 +0000

    Files for week 3 homework

---
+++ week3/homework.md
@@ -0,0 +1,23 @@
+# Week 3 Homework
+
+## Question 1: What is the count for fhv vehicles data for year 2019
+42084899
+
+## Question 2: How many distinct dispatching_base_num we have in fhv for 2019
+792
+
+## Question 3: Best strategy to optimise if query always filter by dropoff_datetime and order by dispatching_base_num
+Partition by dropoff_datetime and cluster by dispatching_base_num
+
+## Question 4: What is the count, estimated and actual data processed for query which counts trip between 2019/01/01 and 2019/03/31 for dispatching_base_num B00987, B02060, B02279
+Count: 26558, Estimated data processed: 400 MB, Actual data processed: 155 MB
+
+## Question 5: What will be the best partitioning or clustering strategy when filtering on dispatching_base_num and SR_Flag
+Cluster by dispatching_base_num and SR_Flag
+
+## Question 6: What improvements can be seen by partitioning and clustering for data size less than 1 GB
+- No improvements
+- Can be worse due to metadata
+
+## Question 7: In which format does BigQuery save data
+Columnar
---
+++ week3/homework.sql
@@ -0,0 +1,28 @@
+-- Creating FHV external table from GCS path
+CREATE OR REPLACE EXTERNAL TABLE `composed-facet-339115.trips_data_all.external_fhv_tripdata`
+    OPTIONS(
+        format= "parquet",
+        uris = [
+        "gs://dtc_data_lake_composed-facet-339115/raw/fhv_tripdata/2019/*"
+        ]
+    );
+
+-- Question 1: What is the count for fhv vehicles data for year 2019
+SELECT COUNT(*) as no_of_trips
+    FROM composed-facet-339115.trips_data_all.external_fhv_tripdata;
+
+-- Question 2: How many distinct dispatching_base_num we have in fhv for 2019
+SELECT COUNT(DISTINCT(dispatching_base_num)) AS no_of_distinct_dbm
+    FROM composed-facet-339115.trips_data_all.external_fhv_tripdata;
+
+-- Creating a partitioned-clustered table
+CREATE OR REPLACE TABLE composed-facet-339115.trips_data_all.fhv_tripdata_partitoned_clustered
+    PARTITION BY DATE(pickup_datetime)
+    CLUSTER BY dispatching_base_num AS
+        SELECT * FROM composed-facet-339115.trips_data_all.external_fhv_tripdata;
+
+-- Question 4: What is the count, estimated and actual data processed for query which counts trip between 2019/01/01 and 2019/03/31 for dispatching_base_num B00987, B02060, B02279
+SELECT COUNT(*) AS count_specific
+    FROM composed-facet-339115.trips_data_all.fhv_tripdata_partitoned_clustered
+    WHERE DATE(pickup_datetime) BETWEEN '2019-01-01' AND '2019-03-31'
+        AND dispatching_base_num='B00987' OR dispatching_base_num='B02060' OR dispatching_base_num='B02279'
